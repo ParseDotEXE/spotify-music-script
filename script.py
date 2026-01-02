3484604e3574bb2f541c2e18c751c118a4d672ef
Parsa <parsa.rahimnia@mail.mcgill.ca>
2026-01-02 18:01:07 -0500

    Implement: added the download  feature. The script can now download the audios from youtube urls, and convert them to mp3 audios. It then stores everything in a subdir in the current dir

--- script.py
+++ script.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from youtubesearchpython import VideosSearch
+import yt_dlp
 
 #load env variables
 load_dotenv()
@@ -51,6 +52,8 @@ def main():
             result = search.result()
             video_url = result ['result'][0]['link'] #get the vid link
             
+            #step 4 is to download the songs from the urls
+            download_yt_vids(video_url)
 
     except Exception as e:
         print(f"Connection failed: {e}")
@@ -66,6 +69,25 @@ def GetId(url):
     playlist_parts = playlist_part.split("?")
     playlist_id = playlist_parts[0] #keep the first part
     return playlist_id
+def download_yt_vids(url):
+    try:
+        ydl_opts = {
+            'format': 'bestaudio/best', #download the audio only
+            'outtmpl': 'songsDownloaded/%(title)s.%(ext)s', #save to current dir in a subfolder
+            'noplaylist': True,
+            #process it to mp3 format
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192'
+                }],
+            }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("Download completed successfull! Audios saved to 'songsDownloaded'")
+    except yt_dlp.utils.DownloadError as de:
+        #print the error message
+        print(f"Download error: {str(de)}")
 
 if __name__ == "__main__":
     main()
